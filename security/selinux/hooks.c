e280406f29cda6c390bb7cf9091572a1fc13c2ba
Linus Torvalds <torvalds@linux-foundation.org>
2013-10-04 12:54:11 -0700

    selinux: remove 'flags' parameter from inode_has_perm
    Every single user passes in '0'.  I think we had non-zero users back in
    some stone age when selinux_inode_permission() was implemented in terms
    of inode_has_perm(), but that complicated case got split up into a
    totally separate code-path so that we could optimize the much simpler
    special cases.
    
    See commit 2e33405785d3 ("SELinux: delay initialization of audit data in
    selinux_inode_permission") for example.
    
    Change-Id: I9f17bfe7c581c16b2b1b66630c649d72a08738ae
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1545,8 +1545,7 @@ static int task_has_system(struct task_struct *tsk,
 static int inode_has_perm(const struct cred *cred,
 			  struct inode *inode,
 			  u32 perms,
-			  struct common_audit_data *adp,
-			  unsigned flags)
+			  struct common_audit_data *adp)
 {
 	struct inode_security_struct *isec;
 	u32 sid;
@@ -1565,7 +1564,7 @@ static int inode_has_perm(const struct cred *cred,
 		return -EINVAL;
 	}
 
-	return avc_has_perm_flags(sid, isec->sid, isec->sclass, perms, adp, flags);
+	return avc_has_perm(sid, isec->sid, isec->sclass, perms, adp);
 }
 
 /* Same as inode_has_perm, but pass explicit audit data containing
@@ -1580,7 +1579,7 @@ static inline int dentry_has_perm(const struct cred *cred,
 
 	ad.type = LSM_AUDIT_DATA_DENTRY;
 	ad.u.dentry = dentry;
-	return inode_has_perm(cred, inode, av, &ad, 0);
+	return inode_has_perm(cred, inode, av, &ad);
 }
 
 /* Same as inode_has_perm, but pass explicit audit data containing
@@ -1595,7 +1594,7 @@ static inline int path_has_perm(const struct cred *cred,
 
 	ad.type = LSM_AUDIT_DATA_PATH;
 	ad.u.path = *path;
-	return inode_has_perm(cred, inode, av, &ad, 0);
+	return inode_has_perm(cred, inode, av, &ad);
 }
 
 /* Same as path_has_perm, but uses the inode from the file struct. */
@@ -1607,7 +1606,7 @@ static inline int file_path_has_perm(const struct cred *cred,
 
 	ad.type = LSM_AUDIT_DATA_PATH;
 	ad.u.path = file->f_path;
-	return inode_has_perm(cred, file_inode(file), av, &ad, 0);
+	return inode_has_perm(cred, file_inode(file), av, &ad);
 }
 
 /* Check whether a task can use an open file descriptor to
@@ -1643,7 +1642,7 @@ static int file_has_perm(const struct cred *cred,
 	/* av is zero if only checking access to the descriptor. */
 	rc = 0;
 	if (av)
-		rc = inode_has_perm(cred, inode, av, &ad, 0);
+		rc = inode_has_perm(cred, inode, av, &ad);
 
 out:
 	return rc;
