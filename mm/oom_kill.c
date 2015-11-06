afda78c6de38f9f66eba0955153b380d540d8276
Oleg Nesterov <oleg@redhat.com>
2015-11-05 18:48:23 -0800

    mm/oom_kill: cleanup the "kill sharing same memory" loop
    Purely cosmetic, but the complex "if" condition looks annoying to me.
    Especially because it is not consistent with OOM_SCORE_ADJ_MIN check
    which adds another if/continue.
    
    Change-Id: I72998fd97f3562849fae56d151e867d7cde1326c
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Hillf Danton <hillf.zj@alibaba-inc.com>
    Cc: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Cc: Kyle Walker <kwalker@redhat.com>
    Cc: Stanislav Kozina <skozina@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -617,14 +617,17 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 	 * pending fatal signal.
 	 */
 	rcu_read_lock();
-	for_each_process(p)
-		if (p->mm == mm && !same_thread_group(p, victim) &&
-		    !(p->flags & PF_KTHREAD)) {
-			if (p->signal->oom_score_adj == OOM_SCORE_ADJ_MIN)
-				continue;
-
-			do_send_sig_info(SIGKILL, SEND_SIG_FORCED, p, true);
-		}
+	for_each_process(p) {
+		if (p->mm != mm)
+			continue;
+		if (same_thread_group(p, victim))
+			continue;
+		if (unlikely(p->flags & PF_KTHREAD))
+			continue;
+		if (p->signal->oom_score_adj == OOM_SCORE_ADJ_MIN)
+			continue;
+		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, p, true);
+	}
 	rcu_read_unlock();
 
 	last_victim = jiffies;
