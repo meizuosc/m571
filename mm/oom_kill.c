51b11d9d1b816c8b6fd0a3195b07c287b06d9d31
Chen Jie <chenjie6@huawei.com>
2015-12-11 13:41:00 -0800

    mm/oom_kill.c: avoid attempting to kill init sharing same memory
    It's possible that an oom killed victim shares an ->mm with the init
    process and thus oom_kill_process() would end up trying to kill init as
    well.
    
    This has been shown in practice:
    
    	Out of memory: Kill process 9134 (init) score 3 or sacrifice child
    	Killed process 9134 (init) total-vm:1868kB, anon-rss:84kB, file-rss:572kB
    	Kill process 1 (init) sharing same memory
    	...
    	Kernel panic - not syncing: Attempted to kill init! exitcode=0x00000009
    
    And this will result in a kernel panic.
    
    If a process is forked by init and selected for oom kill while still
    sharing init_mm, then it's likely this system is in a recoverable state.
    However, it's better not to try to kill init and allow the machine to
    panic due to unkillable processes.
    
    [rientjes@google.com: rewrote changelog]
    [akpm@linux-foundation.org: fix inverted test, per Ben]
    Signed-off-by: Chen Jie <chenjie6@huawei.com>
    Signed-off-by: David Rientjes <rientjes@google.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Hillf Danton <hillf.zj@alibaba-inc.com>
    Cc: Ben Hutchings <ben@decadent.org.uk>
    Cc: Li Zefan <lizefan@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Change-Id: I5b573781c077173b3a472ba8282357a31b193557

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -642,6 +642,8 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 			continue;
 		if (unlikely(p->flags & PF_KTHREAD))
 			continue;
+		if (is_global_init(p))
+			continue;
 		if (p->signal->oom_score_adj == OOM_SCORE_ADJ_MIN)
 			continue;
 		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, p, true);
