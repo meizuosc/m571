45dcd2d85a70bc22b2d525c09234db1d3860a2a0
Tim Kryger <tkryger@google.com>
2016-04-08 15:01:44 -0700

    drivers: mediatek: wdt: Reduce unnecessary logs
    Avoid logging every start or stop of the WDT as this clutters the log.
    
    Bug: 27767950
    Change-Id: I29e603b2514392fb1cf2168f89ff105eace6fc8e
    Signed-off-by: Tim Kryger <tkryger@google.com>

--- drivers/misc/mediatek/wdt/mt6735/mtk_wdt.c
+++ drivers/misc/mediatek/wdt/mt6735/mtk_wdt.c
@@ -768,8 +768,6 @@ void mtk_wd_suspend(void)
 
 	mtk_wdt_restart(WD_TYPE_NORMAL);
 
-	aee_sram_printk("[WDT] suspend\n");
-	printk("[WDT] suspend\n");
 }
 
 void mtk_wd_resume(void)
@@ -783,8 +781,6 @@ void mtk_wd_resume(void)
 		
 	}
 
-	aee_sram_printk("[WDT] resume(%d)\n", g_wdt_enable);
-	printk("[WDT] resume(%d)\n", g_wdt_enable);
 }
 	
 
