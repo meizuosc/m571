3c8c2c55150c9ba24c784471f016d6d2fd1c4ad1
Moyster <oysterized@gmail.com>
2016-11-08 16:04:30 +0100

    hardcode LINUX_COMPILE_BY and COMPILE_HOST to linux@linux-user

--- init/version.c
+++ init/version.c
@@ -40,6 +40,7 @@ struct uts_namespace init_uts_ns = {
 EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
+/*
 const char linux_banner[] =
 	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
 	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
@@ -48,3 +49,13 @@ const char linux_proc_banner[] =
 	"%s version %s"
 	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
 	" (" LINUX_COMPILER ") %s\n";
+*/
+const char linux_banner[] =
+	"Linux version " UTS_RELEASE " (" "linux" "@"
+	"linux-user" ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+
+const char linux_proc_banner[] =
+	"%s version %s"
+	" (" "linux" "@" "linux-user" ")"
+	" (" LINUX_COMPILER ") %s\n";
+
