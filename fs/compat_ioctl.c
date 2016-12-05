b2fcfa4dd718a7040ab7316d85ac8892eb342389
Andreas Fenkart <andreas.fenkart@digitalstrom.com>
2016-12-05 10:47:16 +0100

    gcc6: remove overflow warning in bitshift operation
    The final logical and limits the result the 32bit. Adding interim
    logical and to ensure the compiler an overflow never happens
    
    Signed-off-by: Andreas Fenkart <andreas.fenkart@digitalstrom.com>

--- fs/compat_ioctl.c
+++ fs/compat_ioctl.c
@@ -808,7 +808,7 @@ static int compat_ioctl_preallocate(struct file *file,
  * simple reversible transform to make our table more evenly
  * distributed after sorting.
  */
-#define XFORM(i) (((i) ^ ((i) << 27) ^ ((i) << 17)) & 0xffffffff)
+#define XFORM(i) (((i) ^ (((i) & 0x1f) << 27) ^ (((i) & 0x7fff) << 17)) & 0xffffffff)
 
 #define COMPATIBLE_IOCTL(cmd) XFORM(cmd),
 /* ioctl should not be warned about even if it's not implemented.
