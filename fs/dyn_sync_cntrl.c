fdd3da71eb0fae04ef0e67cd02eab371a26449c6
Mister Oyster <oysterized@gmail.com>
2017-09-22 20:55:31 +0200

    fsync: cleanup
    Signed-off-by: Mister Oyster <oysterized@gmail.com>

--- fs/dyn_sync_cntrl.c
+++ fs/dyn_sync_cntrl.c
@@ -24,6 +24,8 @@
 #include <linux/reboot.h>
 #include <linux/writeback.h>
 
+#include <linux/dyn_sync_cntrl.h>
+
 #define DYN_FSYNC_VERSION_MAJOR 1
 #define DYN_FSYNC_VERSION_MINOR 5
 
@@ -33,7 +35,7 @@
  */
 static DEFINE_MUTEX(fsync_mutex);
 
-bool early_suspend_active __read_mostly = false;
+bool early_suspend_active __read_mostly; // false by default
 bool dyn_fsync_active __read_mostly = true;
 
 static ssize_t dyn_fsync_active_show(struct kobject *kobj,
@@ -47,19 +49,17 @@ static ssize_t dyn_fsync_active_store(struct kobject *kobj,
 {
 	unsigned int data;
 
-	if(sscanf(buf, "%u\n", &data) == 1) {
+	if (sscanf(buf, "%u\n", &data) == 1) {
 		if (data == 1) {
-			pr_info("%s: dynamic fsync enabled\n", __FUNCTION__);
+			pr_info("%s: dynamic fsync enabled\n", __func__);
 			dyn_fsync_active = true;
-		}
-		else if (data == 0) {
-			pr_info("%s: dyanamic fsync disabled\n", __FUNCTION__);
+		} else if (data == 0) {
+			pr_info("%s: dyanamic fsync disabled\n", __func__);
 			dyn_fsync_active = false;
-		}
-		else
-			pr_info("%s: bad value: %u\n", __FUNCTION__, data);
+		} else
+			pr_info("%s: bad value: %u\n", __func__, data);
 	} else
-		pr_info("%s: unknown input!\n", __FUNCTION__);
+		pr_info("%s: unknown input!\n", __func__);
 
 	return count;
 }
@@ -89,22 +89,19 @@ static struct kobj_attribute dyn_fsync_version_attribute =
 static struct kobj_attribute dyn_fsync_earlysuspend_attribute =
 	__ATTR(Dyn_fsync_earlysuspend, 0444, dyn_fsync_earlysuspend_show, NULL);
 
-static struct attribute *dyn_fsync_active_attrs[] =
-	{
+static struct attribute *dyn_fsync_active_attrs[] = {
 		&dyn_fsync_active_attribute.attr,
 		&dyn_fsync_version_attribute.attr,
 		&dyn_fsync_earlysuspend_attribute.attr,
 		NULL,
 	};
 
-static struct attribute_group dyn_fsync_active_attr_group =
-	{
+static struct attribute_group dyn_fsync_active_attr_group = {
 		.attrs = dyn_fsync_active_attrs,
 	};
 
 static struct kobject *dyn_fsync_kobj;
 
-extern void sync_filesystems(int wait);
 static void dyn_fsync_force_flush(void)
 {
 	sync_filesystems(0);
@@ -128,8 +125,7 @@ static void dyn_fsync_resume(struct early_suspend *p)
 	mutex_unlock(&fsync_mutex);
 }
 
-static struct early_suspend dyn_fsync_early_suspend_handler =
-	{
+static struct early_suspend dyn_fsync_early_suspend_handler = {
 		.suspend = dyn_fsync_suspend,
 		.resume = dyn_fsync_resume,
 	};
@@ -175,15 +171,15 @@ static int dyn_fsync_init(void)
 
 	dyn_fsync_kobj = kobject_create_and_add("dyn_fsync", kernel_kobj);
 	if (!dyn_fsync_kobj) {
-		pr_err("%s dyn_fsync kobject create failed!\n", __FUNCTION__);
+		pr_err("%s dyn_fsync kobject create failed!\n", __func__);
 		return -ENOMEM;
-        }
+	}
 
 	sysfs_result = sysfs_create_group(dyn_fsync_kobj,
 			&dyn_fsync_active_attr_group);
 
-        if (sysfs_result) {
-		pr_info("%s dyn_fsync sysfs create failed!\n", __FUNCTION__);
+	if (sysfs_result) {
+		pr_info("%s dyn_fsync sysfs create failed!\n", __func__);
 		kobject_put(dyn_fsync_kobj);
 	}
 	return sysfs_result;
@@ -204,6 +200,6 @@ module_init(dyn_fsync_init);
 module_exit(dyn_fsync_exit);
 
 MODULE_AUTHOR("Paul Reioux <reioux@gmail.com>");
-MODULE_DESCRIPTION("dynamic fsync - automatic fs sync optimizaition using"
+MODULE_DESCRIPTION("dynamic fsync - automatic fs sync optimization using"
 		"early_suspend driver!");
 MODULE_LICENSE("GPL v2");
---
+++ include/linux/dyn_sync_cntrl.h
@@ -0,0 +1,6 @@
+/*
+ * dyn_sync_cntrl.h
+ * Fsync dynamic control header
+ */
+
+extern void sync_filesystems(int wait);
