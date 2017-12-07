e17fca6a8459d306dc40ab203c52f2f165234417
Mister Oyster <oysterized@gmail.com>
2017-12-07 20:13:59 +0100

    drivers: mtk: move to /vendor/etc/firmware
    partially revert moving wifi to vendor/firmware :
    
    https://github.com/Moyster/android_kernel_m2note/commit/7bafe483fbd70668252f724ec04fa950fec4db3f

--- drivers/misc/mediatek/wmt_ccci/wmt_cfg_parser.h
+++ drivers/misc/mediatek/wmt_ccci/wmt_cfg_parser.h
@@ -56,7 +56,7 @@ extern unsigned int wmtCcciLogLvl;
 #endif
 
 #define WMT_CFG_FILE "WMT_SOC.cfg"
-#define WMT_CFG_FILE_PREFIX "/system/vendor/firmware/"
+#define WMT_CFG_FILE_PREFIX "/vendor/etc/firmware/"
 
 /*******************************************************************************
 *                             D A T A   T Y P E S
